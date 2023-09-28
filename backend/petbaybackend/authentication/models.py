a53dce3297e10759574ceaefe5718e68d0b11b62
Aroosh Ahmad <arooshahmad025@gmail.com>
2023-09-28 04:49:39 -0500

    Refactoring
    Refactored the code and stuff, models refactored, views refactored.

--- backend/petbaybackend/authentication/models.py
+++ backend/petbaybackend/authentication/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
+
 # Create your models here.
+
+class LoginUser(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+    class Meta:
+        abstract = True
+
+class User(AbstractBaseUser):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50,unique=True)
+    password = models.CharField(max_length=50)
+
+    REQUIRED_FIELDS = ["password"]
+    USERNAME_FIELD = "email"
+
+    list_display = ("id","name","email")
