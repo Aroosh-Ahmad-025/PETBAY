7b40d3421346bf212ff55f86a0e69cc32dbca7c0
Aroosh Ahmad <arooshahmad025@gmail.com>
2023-10-01 02:46:38 -0500

    Pet Price and Feed Models
    Created Pet Feed and Pet Type models.
    Created a separate model for Price so that, can keep the record if the user continuously keeps changing the price of pet. Can be helpful for analytics

--- backend/petbaybackend/pets/models/pet_models.py
+++ backend/petbaybackend/pets/models/pet_models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from authentication.models import User
+from ..constants import CURRENCY_CHOICES
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 class PetType(models.Model):
@@ -43,5 +44,36 @@ class PetBreed(models.Model):
     petbreed_id = models.AutoField(primary_key=True,db_column="id")
     name = models.CharField(max_length=200)
     pettype_id = models.ForeignKey(PetType,on_delete=models.CASCADE)
+
+class PetPrice(models.Model):
+    """
+        Created a Separate Pet Table to keep track of
+        price changes made by a user.
+        This information can be used to Data Analysis of the website.
+    """
+    id = models.AutoField(primary_key=True)
+    Price = models.IntegerField(
+         validators=[
+            MinValueValidator(1, message="Price must be greater than 0")
+        ]
+    )
+    currency = models.CharField(max_length=3,choices=CURRENCY_CHOICES)
+    Pet = models.ForeignKey(Pet,on_delete=models.CASCADE)
+
+
+class Feed(models.Model):
+    """
+        Contains the Feed Information for a type of pet 
+    """
+
+    id = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=250)
+    Description = models.CharField(max_length=500)
+    price = models.IntegerField(
+        MinValueValidator(1,message="Price must be greater than 0")
+    )
+    currency = models.CharField(max_length=3,choices=CURRENCY_CHOICES)
+    pet_type = models.ForeignKey(PetType,on_delete=models.CASCADE)
+
 
 
